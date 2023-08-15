f1f62b61c961c248dd69fe763455496fae058317
ppluscht <ppluscht@gmail.com>
2023-08-15 17:33:56 -0500

    fixed can i use link

--- src/components/invalidDevice.ts
+++ src/components/invalidDevice.ts
@@ -11,7 +11,7 @@ export class InvalidDevice extends App{
 
         let errorBox = document.createElement("div")
         errorBox.className = "invalid-device-message"
-        errorBox.innerHTML = `Your browser is incompatible with this website. Currently, only the newest versions of edge and chrome for desktop support web gpu (look <a href="https://github.com/PPLUSCHT/LBM" target="_blank">here</a> to see current browser support). Click <a href="https://github.com/PPLUSCHT/LBM" target="_blank">here</a> to see the source code. `
+        errorBox.innerHTML = `Your browser is incompatible with this website. Currently, only the newest versions of edge and chrome for desktop support web gpu (look <a href="https://caniuse.com/webgpu" target="_blank">here</a> to see current browser support). Click <a href="https://github.com/PPLUSCHT/LBM" target="_blank">here</a> to see the source code. `
 
         errorContainer.appendChild(errorBox)
         document.body.appendChild(errorContainer)
